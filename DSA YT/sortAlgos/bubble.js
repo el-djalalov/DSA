dd82c3a279b7231dcca1aa0574729752416709ae
ElyorDjalalov7 <elyor.djalalov@sharp-dev.net>
2025-08-31 19:50:14 +0500

    bubble sort update

--- DSA YT/sortAlgos/bubble.js
+++ DSA YT/sortAlgos/bubble.js
@@ -4,8 +4,22 @@
   Repeat the the instruction as you step thougth each element in the array
   Once you step through the whole array with no swaps, the array is sorted
   */
+// O (n^2)
 
-function bobbleSort(arr) {
+function bubbleSort(arr) {
+	for (let i = arr.length - 1; i > 0; i--) {
+		for (let j = 0; j < i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				let temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+
+	return arr;
+}
+function bobbleSort2(arr) {
 	let swapped;
 	do {
 		swapped = false;
@@ -21,8 +35,6 @@ function bobbleSort(arr) {
 	} while (swapped);
 }
 
-// O (n^2)
-
 const arr = [8, 20, 6, -7, -2, 1];
-bobbleSort(arr);
+bubbleSort(arr);
 console.log(arr);
